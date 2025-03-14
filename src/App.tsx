f5964a2e9d71fec744bba88964968f4cc22fc07c
cactusballs <lottielucyjudge@gmail.com>
2025-03-14 18:26:45 +0000

    starting new Fullstack portfolio

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+function App() {
+  return (
+    <main className='darkbg-zinc-900 darktext-gray-100 flex h-screen justify-center bg-blue-200 text-gray-900'>
+      <section className='my-4 max-w-screen-md flex-col space-y-4 p-4'>
+        <h1 className='text-3xl font-bold'>
+          Hiya, I'm Lottie Judge and I love the Internet
+        </h1>
+
+        <section>
+          <h3 className='text-xl font-bold'>
+            a Starter Template for Full-stack React Projects with Routing and Jest 
+          </h3>
+          <h4 className='font-bold'>What this starter includes:</h4>
+
+          <ul className='list-inside list-disc text-sm'>
+            <li>Tanstack Query ✅</li>
+            <li>Axios for networking ✅</li>
+            <li>Eslint ✅</li>
+            <li>Code conventions with and tailwind class sorting with prettier ✅</li>
+            <li>Relative paths ready ✅</li>
+            <li>classNames and tailwind-merge libraries installed with the `cn` function added ✅</li>
+          </ul>
+        </section>
+      </section>  
+    </main>
+  );
+}
+
+export default App;
