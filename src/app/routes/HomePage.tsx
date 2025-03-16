a847c02817ddccba12d43a60a624973792eb4490
cactusballs <lottielucyjudge@gmail.com>
2025-03-16 10:38:34 +0000

    creating Nav/componenets

---
+++ src/app/routes/HomePage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "components/navBar/Nav"
+
+function homePage(){
+  return(
+    <div>
+    <Navbar/>
+    <main>
+      
+    </main>
+    </div>
+  )
+}
+
+export default homePage;
